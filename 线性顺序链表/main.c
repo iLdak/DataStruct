d1f14cb492940eb83ec3c1c9444871870e4e12a1
iLdak <ildak_linux@163.com>
2015-05-03 15:15:22 +0800

    2015050

--- 线性顺序链表/main.c
+++ 线性顺序链表/main.c
@@ -1,62 +1,63 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "TseqList.h"
+#include <stdio.h> 
+#include <stdlib.h> 
+#include "TseqList.h" 
 
-int main(int argc, char *argv[])
-{
-  seqList* list = SeqList_Create(5);
-  getchar();
-  
-  printf("Capacity:%d\n",SeqList_Capacity(list));
-  getchar();
-  
-  printf("Length:%d\n",SeqList_Length(list));
-  getchar();
-  
-  printf("input 1:%d\n",SeqList_Insert(list, "50", 2));
-  getchar();
-  
-  printf("input 2:%d\n",SeqList_Insert(list, "200", 1));
-  getchar();
-  
-  printf("input 3:%d\n",SeqList_Insert(list, "100", 1));
-  getchar();
-  
-  printf("Length:%d\n",SeqList_Length(list));
-  getchar();
+int main(int argc, char *argv[]) 
+{ 
+   seqList* list = SeqList_Create(5); 
+   getchar(); 
     
-  printf("output 2:%s\n",SeqList_Get(list, 1));
-  getchar();
-  
-  printf("output 1:%s\n",SeqList_Get(list, 0));
-  getchar();
-  
-  printf("output 3:%s\n",SeqList_Get(list, 2));
-  getchar();
-  
-  printf("Delete 2:%s\n",SeqList_Delete(list, 1));
-  getchar();
-  
-  printf("Length:%d\n",SeqList_Length(list));
-  getchar();
-  
-  printf("output 2:%s\n",SeqList_Get(list, 1));
-  getchar();
-  
-  printf("output 1:%s\n",SeqList_Get(list, 0));
-  getchar();
-  
-  SeqList_Clear(list);
-  getchar();
-  
-  printf("Length:%d\n",SeqList_Length(list));
-  getchar();
-  
-  SeqList_Destroy(list);
-  
-  
-  printf("Press enter to continue ...");
-  getchar();	
-  return 0;
-
-}
+   printf("Capacity:%d\n",SeqList_Capacity(list)); 
+   getchar(); 
+    
+   printf("Length:%d\n",SeqList_Length(list)); 
+   getchar(); 
+    
+   printf("input 1:%d\n",SeqList_Insert(list, "50", 2)); 
+   getchar(); 
+    
+   printf("input 2:%d\n",SeqList_Insert(list, "200", 1)); 
+   getchar(); 
+    
+   printf("input 3:%d\n",SeqList_Insert(list, "100", 1)); 
+   getchar(); 
+    
+   printf("Length:%d\n",SeqList_Length(list)); 
+   getchar(); 
+      
+   printf("output 2:%s\n",SeqList_Get(list, 1)); 
+   getchar(); 
+    
+   printf("output 1:%s\n",SeqList_Get(list, 0)); 
+   getchar(); 
+    
+   printf("output 3:%s\n",SeqList_Get(list, 2)); 
+   getchar(); 
+    
+   printf("Delete 2:%s\n",SeqList_Delete(list, 1)); 
+   getchar(); 
+    
+   printf("Length:%d\n",SeqList_Length(list)); 
+   getchar(); 
+    
+   printf("output 2:%s\n",SeqList_Get(list, 1)); 
+   getchar(); 
+    
+   printf("output 1:%s\n",SeqList_Get(list, 0)); 
+   getchar(); 
+    
+   SeqList_Clear(list); 
+   getchar(); 
+    
+   printf("Length:%d\n",SeqList_Length(list)); 
+   getchar(); 
+    
+   SeqList_Destroy(list); 
+    
+    
+   printf("Press enter to continue ..."); 
+   getchar();	 
+   return 0; 
+ 
+ 
+} 
